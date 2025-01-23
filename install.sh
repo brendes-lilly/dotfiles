b5c6c611cd12a933441849a1396640a7c140779b
Brendan Desmond <brendan.desmond@network.lilly.com>
2025-01-23 10:52:21 -0500

    try not to clobber original dotfiles

--- install.sh
+++ install.sh
@@ -15,8 +15,12 @@ for file in \
   zshenv \
   zshrc
 do
-  echo "Backing up ${HOME}/.${file} to ${backup_dir}/.${file}.bak ..."
-  cp "${HOME}/.${file}" "${backup_dir}/.${file}.bak"
-  echo "Linking ${HOME}/.${file} to ${dotfiles}/${file} ..."
-  ln -sf "${dotfiles}/${file}" "${HOME}/.${file}"
+    if [ -e "${backup_dir}/.${file}" ]; then
+      echo "${backup_dir}/.${file} appears to exist already. Manually symlink your updated file and be careful not to clobber the original file installed on the system."
+    else
+      echo "Backing up ${HOME}/.${file} to ${backup_dir}/.${file}.bak ..."
+      cp "${HOME}/.${file}" "${backup_dir}/.${file}.bak"
+      echo "Linking ${HOME}/.${file} to ${dotfiles}/${file} ..."
+      ln -sf "${dotfiles}/${file}" "${HOME}/.${file}"
+    fi
 done

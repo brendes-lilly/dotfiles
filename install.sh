0dc15516184f685bb799cfde9e131526bde0362c
Brendan Desmond <brendan.desmond@network.lilly.com>
2025-01-23 10:02:54 -0500

    use clang

--- install.sh
+++ install.sh
@@ -18,10 +18,3 @@ do
   echo "Linking ${HOME}/.${file} to ${dotfiles}/${file} ..."
   ln -sf "${dotfiles}/${file}" "${HOME}/.${file}"
 done
-
-if command -v zsh >/dev/null 2>&1; then
-  if [ "$SHELL" != "$(command -v zsh)" ]; then
-    echo "Changing default shell to zsh"
-    sudo chsh -s "$(command -v zsh)" "$(whoami)"
-  fi
-fi

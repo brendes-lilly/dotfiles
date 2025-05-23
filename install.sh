0bb0fe5ea10ab8c70f312cd0382d2da7d4bd6a2e
Brendan Desmond <brendan.desmond@network.lilly.com>
2025-05-22 23:09:57 -0400

    Cleanup and improve install

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ backup_dir="${HOME}/backup"
 mkdir -p "${backup_dir}"
 
 for t in ghostty kitty; do
-  command -v tic >/dev/null 2>&1 && tic -x terminfo/${t}.terminfo || echo "tic not found, skipping terminfo"
+  command -v tic >/dev/null 2>&1 &&
+    tic -x terminfo/${t}.terminfo ||
+    echo "tic not found, skipping terminfo"
 done
 
 for file in \
@@ -22,12 +24,13 @@ for file in \
   zshenv \
   zshrc
 do
-    if [ -e "${backup_dir}/.${file}.bak" ]; then
-      echo "${backup_dir}/.${file}.bak appears to exist already. Manually symlink your updated file and be careful not to clobber the original file installed on the system."
-    else
-      echo "Backing up ${HOME}/.${file} to ${backup_dir}/.${file}.bak ..."
-      cp "${HOME}/.${file}" "${backup_dir}/.${file}.bak"
-      echo "Linking ${HOME}/.${file} to ${dotfiles}/${file} ..."
-      ln -sf "${dotfiles}/${file}" "${HOME}/.${file}"
-    fi
+  if [ -e "${backup_dir}/.${file}.bak" ]; then
+    echo "Linking ${HOME}/.${file} to ${dotfiles}/${file} ..."
+    ln -sf "${dotfiles}/${file}" "${HOME}/.${file}"
+  else
+    echo "Backing up ${HOME}/.${file} to ${backup_dir}/.${file}.bak ..."
+    cp "${HOME}/.${file}" "${backup_dir}/.${file}.bak"
+    echo "Linking ${HOME}/.${file} to ${dotfiles}/${file} ..."
+    ln -sf "${dotfiles}/${file}" "${HOME}/.${file}"
+  fi
 done

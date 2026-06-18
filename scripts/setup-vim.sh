d056f4a8c86d64b64d419fe711768a71b32e0738
Brendan Desmond <brendan.desmond@network.lilly.com>
2026-06-18 19:18:58 -0400

    fix again

--- scripts/setup-vim.sh
+++ scripts/setup-vim.sh
@@ -7,9 +7,10 @@ script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 # vim < 9 doesn't include certain plugins
 # vim < 9.1.0327 doesn't use XDG_CONFIG_HOME
 
+src=if
 if command -v vim >/dev/null 2>&1; then
-	vim_sync_opt_packs "${xdg_config}/vim" comment helptoc
+	vim_sync_opt_packs "${XDG_CONFIG_HOME}/vim" comment helptoc
 	if ! vim --version 2>/dev/null | grep -q 'XDG_CONFIG_HOME'; then
-		ln -sfn "${xdg_config}/vim" "${HOME}/.vim"
+		ln -sfn "${XDG_CONFIG_HOME}/vim" "${HOME}/.vim"
 	fi
 fi
